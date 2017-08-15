59089ea6a4e8b0d42879fae4225baf39dfae5b25
Yuyan <heart082045@hotmail.com>
2017-08-15 22:47:16 +0800

    HW1:新增計帳本欄位顯示，以ViewModel及程式預設值帯入。

---
+++ AccountingBook/AccountingBook/Controllers/AccountingController.cs
@@ -0,0 +1,27 @@
+﻿using AccountingBook.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountingBook.Controllers
+{
+    public class AccountingController : Controller
+    {
+        // GET: Accounting
+        public ActionResult Accounting()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            var AccList = new List<AccountingViewModel>();
+            AccList.Add(new AccountingViewModel { Seq = 1, Date = new DateTime(2016, 1, 1), Type = "支出", Amount = 300, Note = "" });
+            AccList.Add(new AccountingViewModel { Seq = 1, Date = new DateTime(2016, 1, 2), Type = "支出", Amount = 1600, Note = "" });
+            AccList.Add(new AccountingViewModel { Seq = 1, Date = new DateTime(2016, 1, 3), Type = "支出", Amount = 800, Note = "" });
+            return View(AccList);
+        }
+    }
+}
---
+++ AccountingBook/AccountingBook/Models/ViewModels/AccountingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AccountingBook.Models.ViewModels
+{
+    public class AccountingViewModel
+    {
+        public int Seq { get; set; }
+        public string Type { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public int Amount { get; set; }
+        public string Note { get; set; }
+    }
+}
