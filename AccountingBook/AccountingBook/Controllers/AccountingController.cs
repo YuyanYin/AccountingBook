fd1b2fd4112c677ade157979cd9461444b7bba64
Yuyan <heart082045@hotmail.com>
2017-08-17 22:28:19 +0800

    新增DB及分頁顯示

--- AccountingBook/AccountingBook/Controllers/AccountingController.cs
+++ AccountingBook/AccountingBook/Controllers/AccountingController.cs
@@ -4,24 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AccountingBook.Models;
+using AccountingBook.Service;
+using PagedList;
 
 namespace AccountingBook.Controllers
 {
     public class AccountingController : Controller
     {
+        private readonly AccountingService _accountingSvc;
+
+        private int pagesize = 25;
+
+        public AccountingController()
+        {
+            _accountingSvc = new AccountingService();
+        }
         // GET: Accounting
         public ActionResult Accounting()
         {
             return View();
         }
 
-        public ActionResult Detail()
+        public ActionResult Detail(int Page = 1)
         {
-            var AccList = new List<AccountingViewModel>();
-            AccList.Add(new AccountingViewModel { Seq = 1, Date = new DateTime(2016, 1, 1), Type = "支出", Amount = 300, Note = "" });
-            AccList.Add(new AccountingViewModel { Seq = 1, Date = new DateTime(2016, 1, 2), Type = "支出", Amount = 1600, Note = "" });
-            AccList.Add(new AccountingViewModel { Seq = 1, Date = new DateTime(2016, 1, 3), Type = "支出", Amount = 800, Note = "" });
-            return View(AccList);
+            int currentpage = Page < 1 ? 1 : Page;
+            var AccList = (from p in _accountingSvc.getAccountingBook()
+                       select new AccountingViewModel
+                       {
+                           Amount = p.Amounttt,
+                           Type = p.Categoryyy == 1 ? "收入" : "支出",
+                           Date = p.Dateee,
+                           Note = p.Remarkkk
+                       }).OrderBy(p => p.Date).ThenBy(p => p.Type);
+            var result = AccList.ToPagedList(currentpage, pagesize);
+            return View(result);
         }
     }
 }
---
+++ AccountingBook/AccountingBook/Models/AccountingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AccountingBook.Models;
+
+namespace AccountingBook.Service
+{
+    public class AccountingService
+    {
+        private readonly SkillTreeHomeworkEntities _db;
+
+        public AccountingService()
+        {
+            _db = new SkillTreeHomeworkEntities();
+        }
+        public IQueryable<AccountBook> getAccountingBook()
+        {
+            return _db.AccountBook;
+        }
+    }
+}
